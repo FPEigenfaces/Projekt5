2ffa163abe963ee72853b56eb860f7894b99ac68
ahmet.celik <ahmet2.celik@tu-dortmund.de>
2020-06-20 17:13:35 +0200

    fetching scaling stuff

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
+import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
+from skimage import io
+from skimage import color
+import numpy as np
+from PIL import Image
 from scipy import misc
 import glob
+import imageio
+
+# fetching images from ./resources and converting them into grayscale
+
+
+def fecth_and_convert():
+    list_np_arrays = []
+    for image_path in glob.glob("./resources/*.png"):
+        img_open = Image.open(image_path)
+        gray = img_open.convert('LA')
+        np_arr = np.array(gray)
+        # gray.save('./resources/gray_scale/%s' %os.path.basename(image_path)) optional
+        list_np_arrays.append(np_arr)
+
+    np_arrs = np.asarray(list_np_arrays)
+
+    return np_arrs
+
+def display(arr):
+    for img in arr:
+        images = Image.fromarray(img)
+        plt.imshow(images, interpolation='nearest')
+        plt.show()
+        break
+
+arrs = fecth_and_convert()
+display(arrs)
+
