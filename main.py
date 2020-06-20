35ae3c6c8b99aa3d46f0d0a34f3cb003e05f6a74
ahmet.celik <ahmet2.celik@tu-dortmund.de>
2020-06-20 17:53:14 +0200

    added cv2

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ from PIL import Image
 from scipy import misc
 import glob
 import imageio
-
+import cv2
 # fetching images from ./resources and converting them into grayscale
 
 
 def fecth_and_convert():
     list_np_arrays = []
     for image_path in glob.glob("./resources/*.png"):
-        img_open = Image.open(image_path)
-        gray = img_open.convert('LA')
-        np_arr = np.array(gray)
+        img_open = cv2.imread(image_path)
+        gray = cv2.cvtColor(img_open, cv2.COLOR_BGR2GRAY)
+        list_np_arrays.append(gray)
         # gray.save('./resources/gray_scale/%s' %os.path.basename(image_path)) optional
-        list_np_arrays.append(np_arr)
+        list_np_arrays.append(gray)
 
     np_arrs = np.asarray(list_np_arrays)
 
     return np_arrs
+
 
 def display(arr):
     for img in arr:
@@ -31,6 +32,6 @@ def display(arr):
         plt.show()
         break
 
+
 arrs = fecth_and_convert()
 display(arrs)
-
